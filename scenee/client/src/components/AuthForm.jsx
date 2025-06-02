65d0c4a6fdee4040445ec86838170660fd1d2ece
achievebin <achieve7384@gmail.com>
2025-06-02 13:43:28 +0900

    RegisterPage

--- scenee/client/src/components/AuthForm.jsx
+++ scenee/client/src/components/AuthForm.jsx
@@ -1,8 +1,9 @@
 // src/components/AuthForm.jsx
 import React, { useState } from 'react';
 
-export default function AuthForm({mode = 'login', onSubmit}) {
-  const [formData, setFormData] = useState({username: '', password: ''});
+export default function AuthForm({mode = 'login', onSubmit, registerField = []}) {
+  const [formData, setFormData] = useState({username: '', password: '', ...Object.fromEntries(registerField.map(f => [f.name, '']))});
+  //...Object.fromEntries(registerField.map(f => [f.name, ''])): registerField에 들어 있는 필드(nickname, email)의 값을 초기화화
   const [error, setError] = useState(null);
 
   const handleChange = (e) => {
@@ -11,6 +12,7 @@ export default function AuthForm({mode = 'login', onSubmit}) {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    //이벤트의 기본 동작을 막는 함수 
     try {
       setError(null);
       await onSubmit(formData);
@@ -29,6 +31,9 @@ export default function AuthForm({mode = 'login', onSubmit}) {
       <div>
         <input name='password' placeholder="비밀번호" type="password" value={formData.password} onChange={handleChange} required/>
       </div>
+      {registerField.map((field)=> (
+        <input key={field.name} {...field} value={formData[field.name]} onChange={handleChange}/>
+        ))}
       <button type='submit'>{mode === 'login' ? '로그인' : '회원가입'}</button>
     </form>
   );
