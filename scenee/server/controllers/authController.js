d7f6e3f21bfa746385d931f6749f998d4ec938c2
achievebin <achieve7384@gmail.com>
2025-06-02 09:16:14 +0900

    authController
    await createUser mariadb bigint 변환 오류 수정

--- scenee/server/controllers/authController.js
+++ scenee/server/controllers/authController.js
@@ -19,12 +19,12 @@ export const registerUser = async (req, res) => {
         if (existingUser) {
         return res
         .status(409)   
-        .json({ message: '이미 사용 중인 아이디입니다.' });
+        .json({success: false, message: '이미 사용 중인 아이디입니다.' });
         //http 응답코드 409(Conflict)
     }
         const hashedPw = await bcrypt.hash(password, saltRound);
         const userId = await createUser(username, hashedPw);
-        return res.status(201).json({success: true, message: '회원가입 성공', userId});
+        return res.status(201).json({success: true, message: '회원가입 성공', userId: userId.toString()});
         //http 응답코드 201(Created)
     } catch (err) {
         console.error(err);
@@ -41,16 +41,16 @@ export const loginUser = async (req, res) => {
   }
   try {
     const user = await getUserByUsername(username);
-    if (!user) return res.status(401).json({ message: '등록된 사용자가 아닙니다.' });
+    if (!user) return res.status(401).json({success: false, message: '등록된 사용자가 아닙니다.' });
     const ok = await bcrypt.compare(password, user.password);
-    if (!ok)  return res.status(401).json({ message: '비밀번호가 일치하지 않습니다.' });
+    if (!ok)  return res.status(401).json({success: false, message: '비밀번호가 일치하지 않습니다.' });
 
     const token = jwt.sign(
       { userId: user.id, username: user.username },
       process.env.JWT_SECRET,
       { expiresIn: '1h' }
     );
-    return res.json({ message: '로그인 성공', token });
+    return res.json({success: true, message: '로그인 성공', token });
   } catch (err) {
     console.error(err);
     return res.status(500).json({success: false, message: '로그인 실패', error: err.message})
