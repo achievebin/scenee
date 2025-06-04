932651d0c67797e7fd24c98a21739dd7ba52c251
achievebin <achieve7384@gmail.com>
2025-06-04 15:23:34 +0900

    Update App.jsx

--- scenee/client/src/App.jsx
+++ scenee/client/src/App.jsx
@@ -1,12 +1,9 @@
 import { useState } from 'react';
 import './App.css';
 import AppRoutes from './routes';
-import HomePage from './pages/Home/HomePage';
 import { BrowserRouter } from 'react-router-dom';
 
 function App() {
-	const [count, setCount] = useState(0);
-
   //routes 폴더의 index.js에서 지정한 사항을 출력
   return (
     <BrowserRouter>
