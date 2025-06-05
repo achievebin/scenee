01af8882fcb916308fc6d3bd7585045530b8a926
achievebin <achieve7384@gmail.com>
2025-06-05 09:32:06 +0900

    model 수정

--- scenee/server/models/reviewModel.js
+++ scenee/server/models/reviewModel.js
@@ -4,39 +4,65 @@ import pool from '../config/db.js'
 //mariaDB와 연결하는 객체 호출
 
 //영화 ID를 기준 삼아 리뷰 조회
-export async function bringReviewsByMovieId(movieId) {
-    const conn = await pool.getConnection();
-    const rows = await conn.query(
-        'SELECT * FROM reviews WHERE movie_id = ?', [movieId]
-    );
-    conn.release();
-    return rows;
+export async function getReviewsByMovieId(movieId) {
+    let conn;
+    try {
+        conn = await pool.getConnection();
+        const [rows] = await conn.query(
+            'SELECT * FROM reviews WHERE movie_id = ? ORDER BY createdAt DESC', [movieId]
+        );
+        return rows;
+    } catch (err) {
+        throw err;
+    } finally {
+        conn.release();
+    }
 }
 
 //리뷰 생성
 export async function createReview(userId, movieId, content, rating) {
-    const conn = await pool.getConnection();
-    await conn.query(
-        'INSERT INTO reviews (user_id, movie_id, content, rating, created_at) VALUES (?, ?, ?, ?, NOW())',
-        [userId, movieId, content, rating]
-    );
-    conn.release();
+    let conn;
+    try {
+        conn = await pool.getConnection();;
+        const [result] = await conn.query(
+            'INSERT INTO reviews (user_id, movie_id, content, rating, created_at) VALUES (?, ?, ?, ?, NOW())',
+            [userId, movieId, content, rating]
+        );
+        return result.insertId;
+    } catch (err) {
+        throw err;
+    } finally {
+        conn.release();
+    }
 }
 
 //리뷰 수정
 export async function rewriteReview(reviewId, userId, movieId, content, rating) {
-    const conn = await pool.getConnection();
-    await conn.query(
+    let conn;
+    try {
+        const [result] = await conn.query(
         'UPDATE reviews SET (content, rating, updated_at) WHERE id = ? AND user_id = ?', [movieId, content, rating, reviewId, userId]
-    );
-    conn.release();
+        );
+        return result.affectedRows;
+    } catch (err) {
+        throw err;
+    } finally {
+        conn.release();
+    }
 }
 
 //리뷰 삭제
 export async function deleteReview(reviewId, userId) {
-    const conn = await pool.getConnection();
-    await conn.query(
-        'DELETE FROM reviews WHERE id = ? AND user_id = ?', [reviewId, userId]
-    );
-    conn.release();
+    let conn;
+    try {
+        conn = await pool.getConnection();
+        const [result] = await conn.query(
+            'DELETE FROM reviews WHERE id = ? AND user_id = ?', [reviewId, userId]
+        );
+        return result.affectedRows;
+    } catch (err) {
+        throw err;
+    } finally {
+        conn.release();
+    }
 }
--- scenee/server/models/userModel.js
+++ scenee/server/models/userModel.js
@@ -12,28 +12,46 @@ export const createUser = async (username, hashedPassword, nickname, email) => {
         );
         //성공한다면 이용자의 ID를 반환합니다.
         return result.insertId;
+    } catch (err) {
+      throw err;
     } finally {
-        //연결 해제
-        conn.release();
+      //연결 해제
+      conn.release();
     }
 }
 
 // 이메일로 유저 조회 (아이디 찾기에 사용)
 export async function findUserByEmail(email) {
-  const sql = 'SELECT * FROM users WHERE email = ?';
-  const [rows] = await pool.query(sql, [email]);
-  
-  if (rows && rows.length > 0) {
+  let conn;
+  try {
+    conn = await pool.getConnection();
+    const [rows] = await conn.query(
+      'SELECT * FROM users WHERE email = ?', [email]
+    )
+    if (rows && rows.length > 0) {
     return rows[0];
+    }
+  } catch (err) {
+    throw err;
+  } finally {
+    conn.release();
   }
-  return null;
 }
 
 // 비밀번호 해시 후, DB에 업데이트 (비밀번호 재설정에 사용)
 export async function updateUserPassword(userId, hashedPwd) {
-  const sql = `UPDATE users SET password = ? WHERE id = ?`;
-  const [result] = await pool.query(sql, [hashedPwd, userId]);
-  return result.affectedRows; // 정상 업데이트 시 1 이상 반환
+  let conn;
+  try {
+    conn = await pool.getConnection();
+    const [result] = await conn.query(
+      `UPDATE users SET password = ? WHERE id = ?`, [hashedPwd, userId]
+    );
+    return result.affectedRows; // 정상 업데이트 시 1 이상 반환
+  } catch (err) {
+    throw err;
+  } finally {
+    conn.release();
+  }
 }
 
 //users 테이블에 userId를 사용하여 이용자 조회
