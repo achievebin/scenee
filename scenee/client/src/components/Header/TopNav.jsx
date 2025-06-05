bbe50fb8f7ba2296e961bb4010b0bcef79b694a2
achievebin <achieve7384@gmail.com>
2025-06-05 12:04:49 +0900

    Update TopNav.jsx

--- scenee/client/src/components/Header/TopNav.jsx
+++ scenee/client/src/components/Header/TopNav.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
-import styles from '../components/Home/Header.module.css'
+import styles from '../Home/Header.module.css'
 
 //Header에 들어가는 로그인 회원가입 페이지로 이동할 수 있는 공간을 담은 컴포넌트
 const TopNav = () => {
