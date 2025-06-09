d4366ac1a547bc0b800d62b9341a9c3ee11e856f
achievebin <achieve7384@gmail.com>
2025-06-09 09:41:45 +0900

    Update App.jsx

--- scenee/client/src/App.jsx
+++ scenee/client/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { AuthProvider } from './contexts/AuthContext';
-import AppRoutes from './index';
+import AppRoutes from './routes/index';
 import TopNav from './components/TopNav';
 
 function App() {
